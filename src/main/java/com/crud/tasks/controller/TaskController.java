b95acf5078b4ecb96fad0857b791142f07088052
KOMP <jakub.cieplowicz@wp.pl>
2022-12-14 13:35:17 +0100

    19.2

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,49 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
+        Task task = service.findById(taskId);
+        return taskMapper.mapToTaskDto(task);
+    }
+    @DeleteMapping(value = "{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
+
+
+    }
+    @PutMapping
+    public TaskDto updateTask(TaskDto taskDto) {
+        return new TaskDto(1L, "test title", "test_content");
+    }
+    @PostMapping
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
