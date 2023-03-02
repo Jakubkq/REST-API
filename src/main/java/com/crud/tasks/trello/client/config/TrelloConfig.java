8c1d89701a1d75934c1212804459a9bb0c287bdf
KOMP <jakub.cieplowicz@wp.pl>
2023-03-02 15:39:10 +0100

    22.4

---
+++ src/main/java/com/crud/tasks/trello/client/config/TrelloConfig.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.trello.client.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class TrelloConfig {
+    private String trelloApiEndpoint;
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+    @Value("${trello.app.token}")
+    private String trelloToken;
+    @Value("$trello.app.username")
+    private String trelloUsername;
+
+}
